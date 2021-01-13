cd17faf61b6f32b0bfd4417996dbfb14215b0ed2
Eric Kim <kimericbyung@gmail.com>
2021-01-13 11:27:52 -0800

    Set up development environment

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        Rendering App ...
+      </div>
+    )
+  }
+}
+
+export default App;
