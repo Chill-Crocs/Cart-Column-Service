530de3107c8ee163777c8d5515f2ab8386cc0448
Eric Kim <kimericbyung@gmail.com>
2021-01-15 11:36:42 -0800

    Created unit tests for the app.jsx and rating

---
+++ tests/rating.test.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Rating from '../client/components/Rating';
+
+const rating = {
+  name: 'hello',
+  sales: -1,
+  stars: -1,
+};
+
+const wrapper = mount(<Rating rating={rating} />);
+
+describe('Rating render', () => {
+  test('should have the correct tags', () => {
+    expect(wrapper.find('span')).toExist();
+    expect(wrapper.find('ul')).not.toExist();
+  });
+});
+
+describe('Rating props', () => {
+  test('should have required props', () => {
+    expect(wrapper.find('span')).toExist();
+    expect(wrapper.find('ul')).not.toExist();
+  });
+});
