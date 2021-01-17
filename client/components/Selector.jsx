399f180805a98ef30267f11eb18701b182e5550a
Eric Kim <kimericbyung@gmail.com>
2021-01-17 02:15:21 -0800

    Finished the view of the selectors

--- client/components/Selector.jsx
+++ client/components/Selector.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Selector extends React.Component {
   constructor(props) {
@@ -9,12 +10,53 @@ class Selector extends React.Component {
   }
 
   render() {
+    const { selectors } = this.props;
+    let i = 0;
+    let j = 0;
+    function selectorMap(options) {
+      j += 1;
+      return (
+        <option key={j} className="optionsBox">
+          {options}
+        </option>
+      );
+    }
+    function optionMap(option) {
+      i += 1;
+      return (
+        <div key={i} className="optionsName">
+          {option.name}
+          <br />
+          <select className="options">
+            {option.options.map(selectorMap)}
+          </select>
+        </div>
+      );
+    }
+    const selectorView = selectors.map(optionMap);
+
     return (
-      <div>
-        Selector
+      <div className="selectors">
+        {selectorView}
       </div>
     );
   }
 }
 
 export default Selector;
+
+Selector.propTypes = {
+  selectors: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      options: PropTypes.arrayOf(PropTypes.string),
+    }),
+  ),
+};
+
+Selector.defaultProps = {
+  selectors: [{
+    name: '',
+    options: [],
+  }],
+};
