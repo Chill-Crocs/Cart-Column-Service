3776d0fc812a191ba13ba1a5cb47e666fc48f800
Eric Kim <kimericbyung@gmail.com>
2021-01-20 01:31:19 -0800

    Finished mvp for shipping and return policies

---
+++ client/components/ShippingPolicies.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ShippingPolicies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button type="button">
+          View shop policies
+        </button>
+      </div>
+    );
+  }
+}
+
+export default ShippingPolicies;
